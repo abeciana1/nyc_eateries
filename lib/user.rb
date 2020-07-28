41d498e66d79f5e07281490a6964bf4674ea1df3
junko911 <junkotahara911@gmail.com>
2020-07-27 22:41:03 -0400

    Created write_review method.

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :restaurants, through: :reviews
+
+  def write_review(restaurant)
+    puts "How many stars do you rate this restaurant? Enter *"
+    rating = gets.chomp
+    puts "Please write your review (up to 255 characters)."
+    review = gets.chomp
+    Review.create(star_rating: rating, desc: review, user_id: self, restaurant_id: restaurant.id)
+  end
 end
