37d4288e05ed9a9426926b09d1eeeaec1d66d3f1
junko911 <junkotahara911@gmail.com>
2020-07-28 12:09:33 -0400

    Modified conflict

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ActiveRecord::Base
-    has_many :reviews
-    has_many :users, through: :reviews
+  has_many :reviews
+  has_many :users, through: :reviews
+
+  def show_details
+    credit_card ? cc = "Yes" : cc = "No"
+    reservations ? rsv = "Yes" : rsv = "No"
+    puts "#{name}\nAddress: #{address}\nMenu: #{menu_link}\nWebsite: #{website}\nYelp page: #{yelp_page}\nBusiness hour: #{hours}\nAbout this restaurant: #{about}\nAccept creadit card?: #{cc}\nAccept reservation?: #{rsv}"
+  end
 end
