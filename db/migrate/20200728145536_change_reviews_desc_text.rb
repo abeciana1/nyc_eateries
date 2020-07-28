14b49242c21a6b48c0d2e7c8033c5470e140f8ec
junko911 <junkotahara911@gmail.com>
2020-07-28 14:49:13 -0400

    Change the method to up and down to make migration reversible.

--- db/migrate/20200728145536_change_reviews_desc_text.rb
+++ db/migrate/20200728145536_change_reviews_desc_text.rb
@@ -1,5 +1,9 @@
 class ChangeReviewsDescText < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :reviews, :desc, :text
+  end
+
+  def down
+    change_column :reviews, :desc, :string
   end
 end
