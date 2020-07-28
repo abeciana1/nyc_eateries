4917bb216dca344f237370236b1b6b8a80937d1a
junko911 <junkotahara911@gmail.com>
2020-07-28 14:11:19 -0400

    Delete migration files to solve error.

--- db/migrate/20200728140447_remove_user_restaurant_cuisines.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserRestaurantCuisines < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :cuisines, :user_id
-    remove_column :cuisines, :restaurant_id
-  end
-end
--- db/migrate/20200728162605_drop_cuisines_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCuisinesTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :cuisines
-  end
-end
