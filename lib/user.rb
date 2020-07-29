9b8d8d934acad99ba25a580b3bec7e9fd182ac39
junko911 <junkotahara911@gmail.com>
2020-07-29 14:50:30 -0400

    Remove unneeded? full_name method.

--- lib/user.rb
+++ lib/user.rb
@@ -4,10 +4,6 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :restaurants, through: :reviews
 
-    def full_name
-        "#{self.first_name} #{self.last_name}"        
-    end
-
     def self.login
         puts "Hey, what's your username?"
         username_input = STDIN.gets.chomp
